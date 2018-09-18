934854ac70c1e231c781e80cde732ae2f320cd89
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-09-18 12:19:50 -0700

    Added randomization of numbers for image files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 var x = 0
+var pic1 = 5;
+var pic2 = 5; 
 
 app.set('view engine', 'pug')
 
@@ -15,6 +17,18 @@ setInterval(function(){
     x++
     io.sockets.emit('timer', {x: x})
 }, 1500)
+
+setInterval(function(){
+    pic1 = Math.floor((Math.random() * 10) + 1)
+    pic2 = Math.floor((Math.random() * 10) + 1)
+    while (pic2 == pic1) {
+       pic2 = Math.floor((Math.random() * 10) + 1)  
+    } 
+
+    console.log("The first picture is " + pic1)
+    console.log("The second picture is " + pic2)
+    
+}, 5000)
 
 app.get('/', function(req, res){
     res.render('index')
