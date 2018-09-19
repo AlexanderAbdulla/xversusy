72f428bb2e3756b6529028b2b98f3ded1ca861df
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-09-18 21:55:20 -0700

    Pictures Randomized
    -added 10 pictures
    -added a random function
    -randomized function on the server side
    updates the client with new pictures

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ var io = require('socket.io')(http)
 var x = 0
 var pic1 = 5;
 var pic2 = 5; 
+var picCounter = 0; 
+
+const testFolder = './public/img/';
+const fs = require('fs');
+
 
 app.set('view engine', 'pug')
 
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(__dirname + '/public'))
 
 
 setInterval(function(){
@@ -24,10 +31,27 @@ setInterval(function(){
     while (pic2 == pic1) {
        pic2 = Math.floor((Math.random() * 10) + 1)  
     } 
-
+ 
     console.log("The first picture is " + pic1)
     console.log("The second picture is " + pic2)
+
+    fs.readdirSync(testFolder).forEach(file => {
+        picCounter++;
+        if(picCounter == pic1) {
+            console.log("found pic 1" + pic1 + file)
+            pic1 = "/img/" + file; 
+        }
+
+        if(picCounter == pic2) {
+            console.log("found pic 2" + pic2 + file)
+            pic2 = "/img/" + file;
+        }
+        //console.log("File number" + picCounter + " is called" + file);
+    })
     
+
+    io.sockets.emit('imageSwapper', {pic1: pic1, pic2: pic2})
+    picCounter = 0; 
 }, 5000)
 
 app.get('/', function(req, res){
